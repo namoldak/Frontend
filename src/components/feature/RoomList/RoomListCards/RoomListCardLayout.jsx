c3d94d15ed9621c9851117c750ce3b2902992b5b
123456soobin-choi <sholong56@gmail.com>
2023-01-02 18:26:21 +0900

    💄 Add RoomListCard in RoomList page

---
+++ src/components/feature/RoomList/RoomListCards/RoomListCardLayout.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import RoomListCard from './RoomListCard';
+
+function RoomListCardLayout() {
+  return (
+    <StRoomListCardLayout>
+      <StRoomListCardBox>
+        <RoomListCard />
+        <RoomListCard />
+        <RoomListCard />
+        <RoomListCard />
+      </StRoomListCardBox>
+    </StRoomListCardLayout>
+  );
+}
+
+const StRoomListCardLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 20px;
+  border: 1px solid #000;
+`;
+
+const StRoomListCardBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+  justify-content: center;
+  padding: 40px 140px 40px 140px;
+  margin: 0 100px 0 100px;
+`;
+
+export default RoomListCardLayout;
