30e073e7972c45c80f7cc16ec854955e007c4662
123456soobin-choi <sholong56@gmail.com>
2023-01-03 12:28:46 +0900

    💄 Add custom Input component

---
+++ src/components/common/Input.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Input({ type, placeholder, onChange, value, width }) {
+  return (
+    <StyledInput
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      width={width}
+    />
+  );
+}
+
+export default Input;
+
+const StyledInput = styled.input`
+  width: ${(props) => props.width || '400px'};
+  height: 30px;
+  font-size: 18px;
+  text-indent: 10px;
+  border: 2px solid rgba(55, 55, 55, 0.5);
+  border-radius: 10px;
+
+  &:focus {
+    outline: none;
+    border-color: #ffd440;
+  }
+`;
