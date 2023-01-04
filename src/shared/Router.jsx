7af7f7c91e0edf43f2cd46f54142f4dc8f72731c
이정민 <kkookk55@naver.com>
2023-01-04 16:17:34 +0900

    📝  add react-cookie

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -7,26 +7,17 @@ import GameRoom from '../pages/GameRoom';
 import LandingPage from '../pages/LandingPage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
-<<<<<<< HEAD
-import RoomListTest from '../pages/RoomListTest';
-=======
 import RoomList from '../pages/RoomList';
->>>>>>> 97d1ea306d8aa426b47be13025c7bca0574d5456
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route path="/gameroom/:roomName" element={<GameRoom />} />
-=======
         <Route path="/rooms" element={<RoomList />} />
-        <Route path="/gameroom" element={<GameRoom />} />
->>>>>>> 97d1ea306d8aa426b47be13025c7bca0574d5456
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/roomlist" element={<RoomListTest />} />
       </Routes>
     </BrowserRouter>
   );
