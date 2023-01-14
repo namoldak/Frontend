c97c01559c873ce4adda187b005d26d7796d5543
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-01-15 05:39:59 +0900

    💄 Update GameRoom component UI

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,11 +1,7 @@
 // 외부모듈
 import React from 'react';
-import styled from 'styled-components';
 
 // 내부모듈
-// import GameRoomAll from '../components/feature/GameRoom/UserCardsAndChatBox/GameRoomAll';
-// import ConnectTest from '../components/feature/GameRoom/UserCardsAndChatBox/ConnectTest';
-// import SignalingTest from '../components/feature/GameRoom/UserCardsAndChatBox/SignalingTest';
 import GameRoomRTC from '../components/feature/GameRoom/UserCardsAndChatBox/GameRoomRTC';
 
 function GameRoom() {
@@ -13,9 +9,3 @@ function GameRoom() {
 }
 
 export default GameRoom;
-
-const StGameRoom = styled.div`
-  border: 3px solid black;
-  display: grid;
-  grid-template-rows: 100px 1fr;
-`;
