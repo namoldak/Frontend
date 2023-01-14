fe221063bffc169e844f286338bc1c3dff076fce
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-01-15 05:34:09 +0900

    💄 Reset css, theme 추가 - GlobalStyle : reset css 적용 - theme : 전역으로 사용할 theme 적용

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { CookiesProvider } from 'react-cookie';
 import { Provider } from 'react-redux';
 import App from './App';
+import theme from './styles/theme';
 import store from './redux/config/configStore';
+import Globalstyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <ThemeProvider theme={theme}>
+      <CookiesProvider>
+        <Globalstyle />
+        <App />
+      </CookiesProvider>
+    </ThemeProvider>
   </Provider>,
 );
