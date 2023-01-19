afe64220caa9fb7eb37e6ea155d88be0b16cc70c
Kim <kimmy199535@gmail.com>
2023-01-20 03:28:35 +0900

    🩹 simple fix for logout

--- src/components/feature/landing/MySettingTabs/LogOutTab.jsx
+++ src/components/feature/landing/MySettingTabs/LogOutTab.jsx
@@ -1,19 +1,38 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 // 외부 모듈
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 // 내부 모듈
-import { removeCookie } from '../../../../utils/cookies';
+import { getNicknameCookie, removeCookie } from '../../../../utils/cookies';
 import { instance } from '../../../../api/core/axios';
+import useToast from '../../../../hooks/useToast';
 
 function LogOutTab(loggedIn) {
   const [isLoggedIn, setIsLoggedIn] = useState(loggedIn);
+  const nickname = getNicknameCookie('nickname');
+
+  console.log('nick', nickname);
 
   function onClickLogOut() {
-    removeCookie('my_token', 'nickname');
-    alert('재밌었닭!');
-    setIsLoggedIn(false);
+    if (nickname === undefined) {
+      useToast('로그인 하지도 않았닭!!');
+    } else {
+      removeCookie('my_token', 'nickname');
+      useToast('재밌었닭!');
+      setIsLoggedIn(false);
+    }
   }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.reload();
+    }, 2500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <StLogoutBtnDiv>
       <button onClick={onClickLogOut}>로그아웃</button>
