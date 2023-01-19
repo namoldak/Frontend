156cb97e812dbe7ee98eea42c74fbf500a6fe788
Kim <kimmy199535@gmail.com>
2023-01-19 15:17:44 +0900

    🩹 fix simple delete unnecessay files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // import logo from './logo.svg';
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import Router from './shared/Router';
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <ToastContainer limit={2} />
+    </>
+  );
 }
 
 export default App;
