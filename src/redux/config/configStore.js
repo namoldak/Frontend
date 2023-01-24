226f53e074fbbb723eb1de5121d4ec184e24aecb
Kim <kimmy199535@gmail.com>
2023-01-24 16:12:38 +0900

    🍱 Update: redux middleware for error msgs

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,6 +1,7 @@
 // 외부 모듈
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import errorToastMsg from 'redux/modules/errorToastMsg';
 
 // 내부 모듈
 import rooms from '../modules/roomSlice';
@@ -13,7 +14,7 @@ const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware({
     serializableCheck: false,
-  }),
+  }).concat(errorToastMsg),
 });
 
 export default store;
