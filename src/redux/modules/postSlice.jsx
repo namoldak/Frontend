430eb3021ae0cf099a4546771460e14f1863ad14
Kim <kimmy199535@gmail.com>
2023-01-26 00:30:48 +0900

    ✨Feat: create comment

--- src/redux/modules/postSlice.jsx
+++ src/redux/modules/postSlice.jsx
@@ -9,6 +9,7 @@ const initialState = {
 export const createPost = createAsyncThunk(
   'post/CREATE_POST',
   async (payload, thunkAPI) => {
+    console.log(payload);
     try {
       const formData = new FormData();
       const json = JSON.stringify(payload.post);
@@ -67,6 +68,25 @@ export const readOnePost = createAsyncThunk(
   },
 );
 
+export const createComment = createAsyncThunk(
+  'comment/CREATE_COMMENT',
+  async (payload, thunkAPI) => {
+    console.log('comment payload', payload);
+    // console.log('comment', JSON.stringify(payload.comment));
+    try {
+      const response = await instance.post(
+        `/posts/${payload.id}/comments`,
+        payload,
+      );
+      console.log('comment response', response);
+      return thunkAPI.fulfillWithValue(response.data);
+    } catch (error) {
+      console.log('comment error', error);
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
+
 export const postSlice = createSlice({
   name: 'posts',
   initialState,
@@ -76,6 +96,9 @@ export const postSlice = createSlice({
       state.isLoading = true;
     },
     [createPost.fulfilled]: (state, action) => {
+      console.log(action.payload);
+      console.log(action.payload.id);
+      state.posts.postResponseDtoList.push(action.payload);
       window.location.href = `/posts/${action.payload.id}`;
       state.isLoading = false;
     },
@@ -93,6 +116,9 @@ export const postSlice = createSlice({
     },
     [readPostsByCategory.rejected]: (state, action) => {
       state.error = action.payload;
+    },
+    [createComment.fulfilled]: (state, action) => {
+      console.log(action.payload);
     },
   },
 });
