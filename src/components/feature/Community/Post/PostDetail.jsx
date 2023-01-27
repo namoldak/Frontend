2b652ef98988b00a05d1ccaac253a620dd5a1457
이정민 <kkookk55@naver.com>
2023-01-27 18:21:36 +0900

    ✨ POST CRUD 기능 구현

--- src/components/feature/Community/Post/PostDetail.jsx
+++ src/components/feature/Community/Post/PostDetail.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
 // 내부 모듈
@@ -12,6 +12,8 @@ import settingBtn from 'assets/images/settingBtn.svg';
 import { readOnePost } from 'redux/modules/postSlice';
 import Modal from 'components/common/Modals/BasicModal/Modal';
 import SettingModal from 'components/common/Modals/BasicModal/SettingModal';
+import { getNicknameCookie } from 'utils/cookies';
+import { instance } from 'api/core/axios';
 import CommentList from '../Comment/CommentList';
 import CreateComment from '../Comment/CreateComment';
 
@@ -20,10 +22,30 @@ function PostDetail() {
   const comment = post.commentList;
   const param = useParams();
   const dispatch = useDispatch();
+  const myNickName = getNicknameCookie('nickname');
+  const [isWriter, setIsWriter] = useState(false);
+  const navigate = useNavigate();
 
   const [display, setDisplay] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isSettingModalOn, setIsSettingModalOn] = useState(false);
+
+  async function deletePost() {
+    instance.delete(`/posts/${param.id}`).then((res) => {
+      navigate('/posts/all');
+    });
+  }
+
+  function updatePost() {
+    navigate(`/posts/modify/${param.id}`);
+  }
+  useEffect(() => {
+    if (myNickName === post.nickname) {
+      setIsWriter(true);
+    } else {
+      setIsWriter(false);
+    }
+  }, [myNickName, post.nickname]);
 
   useEffect(() => {
     dispatch(readOnePost(param.id));
@@ -53,43 +75,59 @@ function PostDetail() {
           <img src={settingBtn} alt="설정" />
         </StSettingBtn>
       </StTopBar>
-      <div>{post.title}</div>
-      <div>
-        {post.imageList?.map((image, index) => {
-          return (
-            <div key={index}>
-              <img src={image} alt="이미지" />
-            </div>
-          );
-        })}
-      </div>
-      <div>{post.content}</div>
-      <div>{post.nickname}</div>
-      <div>{post.createdAt}</div>
-      <div>
-        <CreateComment postId={param.id} />
-        <button
-          onClick={() => {
-            setDisplay(!display);
-          }}
-        >
-          {display && '댓글 숨기기'}
-          {!display && `${post.cmtCnt}개의 댓글보기`}
-        </button>
-        {display &&
-          comment.map((i) => {
-            return (
-              <CommentList
-                postId={param.id}
-                key={i.id}
-                id={i.id}
-                comment={i.comment}
-                nickname={i.nickname}
-                createdAt={i.createdAt}
-              />
-            );
-          })}
-      </div>
+      <StPostDetailInner>
+        <StTitleBox>
+          <StTitle>{post.title}</StTitle>
+          <StNickName>{post.nickname}</StNickName>
+          <StCreated>{post.createdAt}</StCreated>
+        </StTitleBox>
+        <StContentBox>
+          {isWriter ? (
+            <StBtnBox>
+              <button onClick={updatePost}>수정하기</button>
+              <button onClick={deletePost}>삭제하기</button>
+            </StBtnBox>
+          ) : (
+            <>작성자 불일치</>
+          )}
+
+          <StImgdiv>
+            {post.imageList?.map((image, index) => {
+              return (
+                <div key={index}>
+                  <img src={image} alt="이미지" />
+                </div>
+              );
+            })}
+          </StImgdiv>
+          <div>{post.content}</div>
+
+          <div>
+            <CreateComment postId={param.id} />
+            <button
+              onClick={() => {
+                setDisplay(!display);
+              }}
+            >
+              {display && '댓글 숨기기'}
+              {!display && `${post.cmtCnt}개의 댓글보기`}
+            </button>
+            {display &&
+              comment.map((i) => {
+                return (
+                  <CommentList
+                    postId={param.id}
+                    key={i.id}
+                    id={i.id}
+                    comment={i.comment}
+                    nickname={i.nickname}
+                    createdAt={i.createdAt}
+                  />
+                );
+              })}
+          </div>
+        </StContentBox>
+      </StPostDetailInner>
     </StPostDetail>
   );
 }
@@ -115,4 +153,37 @@ const StSettingBtn = styled.button`
   height: 78px;
 `;
 
+const StPostDetailInner = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  border-radius: 10px;
+  background-color: black;
+  padding: 20px;
+`;
+const StImgdiv = styled.div`
+  max-width: 300px;
+  max-height: 300px;
+`;
+
+const StContentBox = styled.div`
+  border: 2px solid white;
+  background-color: beige;
+  margin-top: 20px;
+`;
+const StTitleBox = styled.div`
+  border: 2px solid blue;
+  background-color: beige;
+`;
+const StBtnBox = styled.div`
+  float: right;
+`;
+const StTitle = styled.div`
+  display: inline-block;
+`;
+const StNickName = styled.div`
+  display: inline-block;
+`;
+const StCreated = styled.div`
+  display: inline-block;
+`;
 export default PostDetail;
