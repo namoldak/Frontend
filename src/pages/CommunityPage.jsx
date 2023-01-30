e528605d9c283df8317ffe6739c6f2c8c749f27b
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-01-30 17:33:06 +0900

    🎨 Improve : Community page structure

--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -1,22 +1,11 @@
 // 외부 모듈
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 
 // 내부 모듈
 import PostList from 'components/feature/Community/Post/PostList';
-import CommunityTopBar from 'components/feature/Community/CommunityHeader/CommunityTopBar';
 
 function CommunityPage() {
-  return (
-    <StCommunity>
-      <CommunityTopBar />
-      <PostList />
-    </StCommunity>
-  );
+  return <PostList />;
 }
-
-const StCommunity = styled.div`
-  padding-top: 80px;
-`;
 
 export default CommunityPage;
