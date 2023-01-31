9ecd0d582041d3ab58e1c6132283f2bcef765c71
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-02-01 03:46:20 +0900

    💄 Add responsive UI Layout

--- src/components/common/Layout/PostContainer.jsx
+++ src/components/common/Layout/PostContainer.jsx
@@ -14,10 +14,6 @@ const StContainer = styled.div`
   width: 1180px; // 좌우 여백 370px
   height: 100vh;
   margin: 0 auto;
-  /* padding-top: 20px; */
-  @media ${(props) => props.theme.laptop} {
-    width: 1080px;
-  }
 `;
 
 export default Container;
