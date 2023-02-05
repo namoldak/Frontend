413a66ef319e80c7cd6ae40fc7b1713ab9dcc64c
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-02-06 02:09:55 +0900

    🚸 improve UX : change nickname - 닉네임 변경 모달 ver

---
+++ src/components/common/SettingModal/LogOut/ChangeNick.jsx
@@ -0,0 +1,50 @@
+// 외부 모듈
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+// 내부 모듈
+import Modal from 'components/common/Modals/BasicModal/Modal';
+import ChangeNameModal from 'components/common/Modals/BasicModal/ChangeNameModal';
+
+function ChangeNick({ setting }) {
+  const [isModalOn, setIsModalOn] = useState(false);
+  function onClickChange() {
+    setIsModalOn(true);
+  }
+  return (
+    <StChangeNick>
+      <StChangeNickBox>
+        <ChangeUserName onClick={onClickChange}>닉네임 변경</ChangeUserName>
+        {isModalOn && (
+          <Modal
+            onClose={() => {
+              setIsModalOn(false);
+            }}
+            content={<ChangeNameModal setting={setting} />}
+          />
+        )}
+      </StChangeNickBox>
+    </StChangeNick>
+  );
+}
+
+const StChangeNick = styled.div`
+  margin-bottom: 24px;
+`;
+
+const StChangeNickBox = styled.div``;
+
+const ChangeUserName = styled.button`
+  width: 120px;
+  height: 36px;
+  background: ${({ theme }) => theme.colors.lightBrown};
+  ${({ theme }) => theme.common.border};
+  border-radius: 10px;
+  font-weight: 600;
+  font-size: 12px;
+  text-align: center;
+  letter-spacing: 0.22em;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export default ChangeNick;
