6f5d35bc2de6ca56801b370b18ebfc66a32c1634
최수빈 <107169675+123456soobin-choi@users.noreply.github.com>
2023-02-10 13:09:42 +0900

    👔  Fix change nickname logic

--- src/components/common/Input/ChangeNickInput.jsx
+++ src/components/common/Input/ChangeNickInput.jsx
@@ -1,28 +1,17 @@
 // 외부 모듈
-import React from 'react';
+import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 
-// const ChangeNickInput = React.forwardRef((props, ref) => {
-//   return (
-//     <StChangeNickInput
-//       {...props}
-//       forwardedRef={ref}
-//       placeholder="변경할 닉네임을 입력해주세요."
-//     />
-//   );
-// });
-
-function ChangeNickInput() {
-  return <StChangeNickInput placeholder="변경할 닉네임을 입력해주세요." />;
-}
+const ChangeNickInput = forwardRef(({ placeholder, ...rests }, ref) => {
+  return <StChangeNickInput placeholder={placeholder} ref={ref} {...rests} />;
+});
 
 const StChangeNickInput = styled.input`
   width: 198px;
   height: 36px;
-  background: ${({ theme }) => theme.colors.paperGray};
+  background: ${({ theme }) => theme.colors.lightBeige};
   border-radius: 10px;
   margin: 0 14px 0 30px;
-  color: ${({ theme }) => theme.colors.paperGray};
   font-weight: 200;
   font-size: 12px;
   line-height: 56px;
